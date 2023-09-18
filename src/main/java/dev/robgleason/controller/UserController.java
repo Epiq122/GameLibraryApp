2ad60744217385f9672fa921be67997dca889b35
Robert Gleason <l2ob122@gmail.com>
2023-09-17 22:12:59 -0700

    added JWT to the dependencies

--- src/main/java/dev/robgleason/controller/UserController.java
+++ src/main/java/dev/robgleason/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,6 +31,7 @@ public class UserController {
 
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<UserEntityDto> updatedUser(@PathVariable("id") Long userId, @RequestBody UserEntityDto userEntityDto) {
         userEntityDto.setId(userId);
@@ -37,49 +39,56 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<UserEntityDto>> getAllUsers() {
         List<UserEntityDto> allUsers = userService.getAllUsers();
         return ResponseEntity.ok(allUsers);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("{id}")
     public ResponseEntity<UserEntityDto> getUser(@PathVariable("id") Long userId) {
         UserEntityDto userByIdDto = userService.getUserById(userId);
         return ResponseEntity.ok(userByIdDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/username/{username}")
     public ResponseEntity<UserEntityDto> getUserByUsername(@PathVariable("username") String username) {
         UserEntityDto userByUsernameDto = userService.getUserByUsername(username);
         return ResponseEntity.ok(userByUsernameDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/email/{email}")
     public ResponseEntity<UserEntityDto> getUserByEmail(@PathVariable("email") String email) {
         UserEntityDto userByUserEmailDto = userService.getUserByEmail(email);
         return ResponseEntity.ok(userByUserEmailDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/collections/{id}")
     public ResponseEntity<List<CollectionEntityDto>> getCollectionsByUser(@PathVariable("id") Long userId) {
         List<CollectionEntityDto> collections = collectionsService.getCollectionByUser(userId);
         return ResponseEntity.ok(collections);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId) {
         userService.deleteUser(userId);
         return ResponseEntity.ok("user deleted successfully");
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/username/{username}")
     public ResponseEntity<String> deleteUserByUserName(@PathVariable("username") String username) {
         userService.deleteUserByUsername(username);
         return ResponseEntity.ok("user deleted successfully");
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/email/{email}")
     public ResponseEntity<String> deleteUserByEmail(@PathVariable("email") String email) {
         userService.deleteUserByEmail(email);
