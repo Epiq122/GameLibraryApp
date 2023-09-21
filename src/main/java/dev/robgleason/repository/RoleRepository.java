45dc40e6189c5cccc0f74f01f398873d864611c8
Robert Gleason <l2ob122@gmail.com>
2023-09-21 12:18:52 -0700

    Added platform

--- src/main/java/dev/robgleason/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.robgleason.repository;
-
-import dev.robgleason.entity.RoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-
-    RoleEntity findByName(String name);
-}
